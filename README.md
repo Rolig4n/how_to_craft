21b6712783ede258934c955b73a7dfb9e7f0840f
Hiago PC <hiagok.o@gmail.com>
2024-09-06 14:38:54 -0300

    att da ultima aula da imersão, projeto praticamente encerrado

--- README.md
+++ README.md
@@ -6,3 +6,41 @@ Projeto de consulta de crafts do jogo minecraft, desenvolvido na Imersão Alura 
 | -------------  | --- |
 | :sparkles: Nome        | **Imersão-Gemini**
 | :label: Tecnologias | html, css, javascript
+
+## How to craft? - Buscador de Receitas de Minecraft (README.md)
+
+**Criado com ❤️ por Hiago Rodrigues Batista**
+
+Esta é uma aplicação web para te ajudar a encontrar receitas de criação de itens no Minecraft! ⛏️
+
+**Como funciona:**
+
+1. **Barra de Busca:** Digite o nome do item ou bloco que você está procurando na barra de busca.
+2. **Modo de Busca:**
+    - **"Mostrar Tudo"** (padrão): Exibe receitas para todos os itens.
+    - **Termo de Busca:** Filtra receitas com base no seu termo de busca.
+3. **Receitas de Criação:** Os resultados da busca mostram as receitas de criação para os itens correspondentes. Cada receita inclui:
+    - O item ou bloco criado.
+    - Os ingredientes necessários e suas quantidades.
+    - Imagens dos ingredientes e do item resultante.
+
+**Feito com dados via API**
+
+Esta aplicação utiliza dados da API criada por [Anish Shanbhag](https://github.com/anish-shanbhag/minecraft-api), para fornecer as informações sobre as receitas de criação.
+
+**Comece agora!**
+
+1. Clone ou baixe o repositório.
+2. Certifique-se de ter o Node.js e npm instalados.
+3. Execute `npm install` para instalar as dependências.
+4. Abra `index.html` no seu navegador para começar a usar a aplicação.
+
+**Melhorias futuras:**
+
+- Implementar opções de busca avançadas (por exemplo, por ingrediente)
+- Adicionar suporte para filtrar por categoria de item (ferramentas, armas, etc.)
+- Melhorar o design visual e a experiência do usuário
+
+**Faça um fork!**
+
+Este é um projeto open-source e suas contribuições são bem-vindas. Faça um fork do repositório, para as suas alterações e envie um pull request!
--- app.js
+++ app.js
@@ -1,52 +1,96 @@
+let crafting = ""
+let craftingList = ""
+const inputPesquisa = document.getElementById('input_pesquisa');
+const btnText = document.getElementById('btn-text');
+
 function normalizeString(str) {
-    if (Array.isArray(str)) {
-        str = str[0]; // Pega o primeiro elemento da lista
-    }
     if (str === null || str === undefined) {
         return ''; // Retorna uma string vazia ou outro valor padrão
     }
     // Remover espaços em branco
-    str = str.replace(/\s+/g, '_')
-        .normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+    str = str.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
         .toLowerCase();
 
     return str;
 }
 
-let section = document.getElementById("resultados-pesquisa")
-let itens = ""
-let crafting_list = ""
+function getCraftingItemImage(recipe, itemLista) {
+    let recipeList = ""
+    let pesquisa = ""
+    for (let i = 0; i < recipe.length; i++) {
+        if (recipe[i] !== null) {
+            if (Array.isArray(recipe[i])) {
+                pesquisa = recipe[i][0]; // Pega o primeiro elemento da lista caso varias receitas sejam possiveis
+            } else {
+                pesquisa = recipe[i]
+            }
+            const item = itemLista.find(item => item.name === pesquisa);
+            if (item) {
+                recipeList += `<div class="craft slot"><img src="${item.image}"></div>`;
+            }
+        }
+        else {
+            recipeList += `<div class="craft slot"></div>`; // Sem item na posição retorna nada, coloca uma div vazia
+        }
+    }
+    return recipeList
+}
 
-function pesquisar() {
-    dados.then(resultado => {
-        items.then(resultado => {
-            itens = resultado
-        }).catch(resultado => {console.log(resultado)})
-        for (let craft of resultado) {
-            let namespacedId = normalizeString(craft.item)
-            crafting_list += `
+function getItemImage(item, itensLista) {
+    for (let j = 0; j < itensLista.length; j++) {
+        const iten = itensLista.find(iten => iten.name === item);
+        if (iten) {
+            return `<img src="${iten.image}"></img>`;
+        }
+    }
+}
+
+async function pesquisar() {
+    let section = document.getElementById("resultados-pesquisa")
+    let craftingList = ""
+    const itens = await items.then(resultado => {
+        return resultado
+    }).catch(resultado => {console.log(resultado)})
+
+    crafting = await dados.then(resultado => {
+        return resultado
+    }).catch(resultado => {console.log(resultado)})
+
+    for (let craft of crafting) {
+        let namespaceItem = normalizeString(craft.item)
+        let namespacePesquisa = normalizeString(inputPesquisa.value)
+        if (namespaceItem.includes(namespacePesquisa)) { // verifica se item é igual pesquisa
+            craftingList += `
             <div class="item-resultado">
                 <h2>${craft.item}</h2>
                 <div id="craft">
-                    <div class="craft slot"><img src="https://minecraft-api.vercel.app/images/items/${normalizeString(craft.recipe[0])}.png"></div>
-                    <div class="craft slot"><img src="https://minecraft-api.vercel.app/images/items/${normalizeString(craft.recipe[1])}.png"></div>
-                    <div class="craft slot"><img src="https://minecraft-api.vercel.app/images/items/${normalizeString(craft.recipe[2])}.png"></div>
-                    <div class="craft slot"><img src="https://minecraft-api.vercel.app/images/items/${normalizeString(craft.recipe[3])}.png"></div>
-                    <div class="craft slot"><img src="https://minecraft-api.vercel.app/images/items/${normalizeString(craft.recipe[4])}.png"></div>
-                    <div class="craft slot"><img src="https://minecraft-api.vercel.app/images/items/${normalizeString(craft.recipe[5])}.png"></div>
-                    <div class="craft slot"><img src="https://minecraft-api.vercel.app/images/items/${normalizeString(craft.recipe[6])}.png"></div>
-                    <div class="craft slot"><img src="https://minecraft-api.vercel.app/images/items/${normalizeString(craft.recipe[7])}.png"></div>
-                    <div class="craft slot"><img src="https://minecraft-api.vercel.app/images/items/${normalizeString(craft.recipe[8])}.png"></div>
+                    ${getCraftingItemImage(craft.recipe, itens)}
                 </div>
                 <div>
                     <span class="craft-arrow"></span>
                 </div>
                 <div style="align-content: center;">
-                    <div class="craft result">${craft.quantity > 1 ? `<span>${craft.quantity}</span>` : ''}<img src="https://minecraft-api.vercel.app/images/items/${namespacedId}.png"></div>
+                    <div class="craft result">${craft.quantity > 1 ? `<span>${craft.quantity}</span>` : ''}${getItemImage(craft.item, itens)}</div>
                 </div>
             </div>
-        `
+            `
         }
-        section.innerHTML = crafting_list
-    }).catch(resultado => {console.log(resultado)})
+    }
+    if (craftingList.length == 0) {
+        section.innerHTML = "Nothing found"
+    } else {
+        section.innerHTML = craftingList
+    }
 }
+
+function atualizarTextoBotao() {
+    if (inputPesquisa.value.trim() === '') {
+        btnText.textContent = 'Show All';
+    } else {
+        btnText.textContent = 'Search';
+    }
+}
+// Adiciona um event listener ao input
+inputPesquisa.addEventListener('input', atualizarTextoBotao);
+// Chama a função inicialmente para setar o texto correto
+atualizarTextoBotao();
