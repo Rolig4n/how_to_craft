8c31053f8b05ce5d4bffef8c37dcb5428d89e105
hiago rodrigues <ti7@facfama.edu.br>
2024-09-09 17:28:38 -0300

    add slide de imagens para itens com mais de 1 receita

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let crafting = ""
 let craftingList = ""
+let slides = ""
+let slideIndex = 0;
 const inputPesquisa = document.getElementById('input_pesquisa');
 const btnText = document.getElementById('btn-text');
 
@@ -20,13 +22,25 @@ function getCraftingItemImage(recipe, itemLista) {
     for (let i = 0; i < recipe.length; i++) {
         if (recipe[i] !== null) {
             if (Array.isArray(recipe[i])) {
-                pesquisa = recipe[i][0]; // Pega o primeiro elemento da lista caso varias receitas sejam possiveis
+                let subRecipeList = ""
+                for (let j = 0; j < recipe[i].length; j++) {
+                    if (recipe[i][j] !== null) {   
+                        pesquisa = recipe[i][j]
+                        const sub_item = itemLista.find(item => item.name === pesquisa);
+                        if (sub_item) {
+                            subRecipeList += `<img src="${sub_item.image}"></img>`
+                        }
+                    } else {
+                        subRecipeList += ''
+                    }
+                }
+                recipeList += `<div class="craft slot carousel"><div class="slide">${subRecipeList}</div></div>`;
             } else {
                 pesquisa = recipe[i]
-            }
-            const item = itemLista.find(item => item.name === pesquisa);
-            if (item) {
-                recipeList += `<div class="craft slot"><img src="${item.image}"></div>`;
+                const item = itemLista.find(item => item.name === pesquisa);
+                if (item) {
+                    recipeList += `<div class="craft slot"><img src="${item.image}"></div>`;
+                }
             }
         }
         else {
@@ -63,14 +77,16 @@ async function pesquisar() {
             craftingList += `
             <div class="item-resultado">
                 <h2>${craft.item}</h2>
-                <div id="craft">
-                    ${getCraftingItemImage(craft.recipe, itens)}
-                </div>
-                <div>
-                    <span class="craft-arrow"></span>
-                </div>
-                <div style="align-content: center;">
-                    <div class="craft result">${craft.quantity > 1 ? `<span>${craft.quantity}</span>` : ''}${getItemImage(craft.item, itens)}</div>
+                <div class="div-resultado">
+                    <div id="craft">
+                        ${getCraftingItemImage(craft.recipe, itens)}
+                    </div>
+                    <div>
+                        <span class="craft-arrow"></span>
+                    </div>
+                    <div style="align-content: center;">
+                        <div class="craft result">${craft.quantity > 1 ? `<span>${craft.quantity}</span>` : ''}${getItemImage(craft.item, itens)}</div>
+                    </div>
                 </div>
             </div>
             `
@@ -81,6 +97,8 @@ async function pesquisar() {
     } else {
         section.innerHTML = craftingList
     }
+    slides = document.querySelectorAll(".slide")
+    setInterval(nextSlide, 2000)
 }
 
 function atualizarTextoBotao() {
@@ -94,3 +112,18 @@ function atualizarTextoBotao() {
 inputPesquisa.addEventListener('input', atualizarTextoBotao);
 // Chama a função inicialmente para setar o texto correto
 atualizarTextoBotao();
+
+function showSlide(index) {
+    slideIndex = (index + slides.length) % slides.length;
+    slides.forEach(slide => {
+        const images = slide.querySelectorAll('img');
+        images.forEach((image, imgIndex) => {
+            image.classList.toggle('active', imgIndex === index);
+            image.classList.toggle('hidden', imgIndex !== index);
+        });
+    })
+}
+
+function nextSlide() {
+    showSlide(slideIndex + 1);
+}
